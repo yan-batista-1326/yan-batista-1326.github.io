e33793e9220b230ce221dadc623b8bb20623c9bb
Yan Batista <ybatistasouza@hotmail.com>
2020-02-13 23:36:35 -0300

    Add files via upload

--- 07-02-2020/classes.js
+++ 07-02-2020/classes.js
@@ -1,12 +1,24 @@
 class Agenda {
-    constructor() {
-      this.contato = [];
-    }
-    
-    addContato(nome, sobrenome, nascimento, tipo, ddd, num) {
-      this.contato.push(new Contato(new Pessoa(nome, sobrenome, nascimento), new Numero(tipo, ddd, num)));
-    }
+  constructor() {
+    this.contato = [];
   }
+    
+  addContato(nome, sobrenome, nascimento, tipo, ddd, num) {
+    this.contato.push(new Contato(new Pessoa(nome, sobrenome, nascimento), new Numero(tipo, ddd, num)));
+    console.log("Contato adicionado");
+  }
+
+  criarContato() {
+    var nome = window.prompt("Informe o nome");
+    var sobrenome = window.prompt("Informe o sobrenome");
+    var nascimento = window.prompt("Informe a data de nascimento");
+    var tipo = window.prompt("Informe o tipo do telefone");
+    var ddd = window.prompt("Informe o DDD");
+    var numero = window.prompt("Informe o número");
+    console.log(this);
+    this.addContato(nome, sobrenome, nascimento, tipo, ddd, numero);
+  }
+}
 
 class Contato {
     constructor(pessoa, numero) {
@@ -16,7 +28,31 @@ class Contato {
       this.end = [];
       this.email = [];
     }
-       
+    
+    adicionarNovo() {
+      var x = window.prompt("Inserir novo Número(1), Endereço(2), Email(3):");
+      if(x == 1) {
+        var tipo = prompt("Tipo de telefone");
+        var ddd = prompt("DDD");
+        var num = prompt("Numero");
+        this.addNumero(tipo, ddd, num);
+      } else if(x == 2) {
+        var tipo = prompt("Tipo do Endereço");
+        var logradouro = prompt("Logradouro");
+        var num = prompt("Numero");
+        var cep = prompt("CEP");
+        var complemento = prompt("Complemento");
+        this.addEndereco(tipo, logradouro, num, cep, complemento);
+      } else if(x == 3) {
+        var tipo = prompt("Tipo de Email");
+        var email = prompt("Email");
+        this.addEmail(tipo, email);
+      } else {
+        alert("Entre com os número entre 1 e 3");
+        this.adicionarNovo();
+      }
+    }
+    
     addNumero(tipo, ddd, num) {
       this.numero.push(new Numero(tipo, ddd, num));
     }
@@ -57,10 +93,10 @@ class Endereco {
   }
 
 class Email {
-    constructor(tipo, email) {
-      this.tipo = tipo;
-      this.email = email;
-    }
+  constructor(tipo, email) {
+    this.tipo = tipo;
+    this.email = email;
   }
+}
 
-  var agenda = new Agenda();
+var agenda = new Agenda();
